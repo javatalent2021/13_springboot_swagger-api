ae2c3067a2ccb19f3cb54abaa00e63756884c70d
springapp <sangaraju.ramesh@gmail.com>
2024-02-09 00:59:39 +0530

    First Commit

---
+++ src/main/java/com/springboot/bindings/Product.java
@@ -0,0 +1,40 @@
+package com.springboot.bindings;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Product {
+	
+	
+	private Integer productId;
+	private String productName;
+	private String productPrice;
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(String productPrice) {
+		this.productPrice = productPrice;
+	}
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", productName=" + productName + ", productPrice=" + productPrice
+				+ "]";
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/springboot/controller/WelcomeRestController.java
@@ -0,0 +1,87 @@
+package com.springboot.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.bindings.Product;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@Api("this is product details api")
+public class WelcomeRestController {
+	
+	@GetMapping("/welcome")
+	public ResponseEntity<String> getWelcome(){
+		
+		String responsePayload ="Welcome to Ashok It";
+		return new ResponseEntity<>(responsePayload, HttpStatus.OK);
+	}
+	
+	@GetMapping("/greet")
+	public ResponseEntity<String> getWelcome(@RequestParam("name") String name){
+		
+		String responsePayload ="Hi "+name +" Good Morning";
+		return new ResponseEntity<>(responsePayload, HttpStatus.OK);
+	}
+	
+	@GetMapping("/book/{name}/trainer/{price}")
+	public ResponseEntity<String> getWelcome(@PathVariable("name") String name,@PathVariable("price") String price){
+		
+		String responsePayload ="Book Name is : "+name +" And Book Price is :"+price;
+		return new ResponseEntity<>(responsePayload, HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/product", produces= {"application/xml","application/json"})
+	public ResponseEntity<Product> getProduct(){
+		
+		Product product = new Product();
+		product.setProductId(101);
+		product.setProductName("Samsung");
+		product.setProductPrice("200");
+		
+		return new ResponseEntity<>(product, HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/products", produces= {"application/xml","application/json"})
+	public ResponseEntity<List<Product>> getProductList(){
+		
+		Product product = new Product();
+		product.setProductId(101);
+		product.setProductName("Samsung");
+		product.setProductPrice("200");
+		
+		Product product1 = new Product();
+		product1.setProductId(102);
+		product1.setProductName("Iphone");
+		product1.setProductPrice("300");
+		
+		Product product2 = new Product();
+		product2.setProductId(103);
+		product2.setProductName("Redmi");
+		product2.setProductPrice("250");
+		
+		return new ResponseEntity<>(Arrays.asList(product,product1,product2), HttpStatus.OK);
+	}
+	
+	@PostMapping(value="/product", consumes= {"application/xml","application/json"})
+	public ResponseEntity<String> saveProdcut(@RequestBody Product product){
+		
+		System.out.println(product);
+		
+		String msg = "Successfully inserted product record";
+		return new ResponseEntity<>(msg, HttpStatus.CREATED);
+	}
+	
+	
+
+}
